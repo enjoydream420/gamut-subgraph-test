6d069ae853bc8baf9e28f743f21ec4bfe5bbbffb
enjoydream420 <cosmodream93@gmail.com>
2023-02-22 22:06:50 -0800

    test

---
+++ src/gamut-factory.ts
@@ -0,0 +1,36 @@
+import {
+  OwnershipTransferred as OwnershipTransferredEvent,
+  PoolCreated as PoolCreatedEvent
+} from "../generated/GamutFactory/GamutFactory"
+import { OwnershipTransferred, PoolCreated } from "../generated/schema"
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePoolCreated(event: PoolCreatedEvent): void {
+  let entity = new PoolCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.token0 = event.params.token0
+  entity.token1 = event.params.token1
+  entity.pool = event.params.pool
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
